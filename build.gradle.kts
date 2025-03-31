e92c8ea107a09aa0765e014c02777381b5e4c8b7
eoancuta <eoancuta@gmail.com>
2025-04-01 00:33:31 +0300

    feat: cleaned up dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,21 +12,15 @@ repositories {
 dependencies {
     // Kotlin Coroutines for async operations
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-//    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 
+    // CLIENTS
     // Lettuce (Reactive Redis Client)
-    implementation("io.lettuce:lettuce-core:6.5.0.RELEASE")
-
+    implementation("io.lettuce:lettuce-core:6.5.5.RELEASE")
     // Redisson (Reactive Redis Client)
     implementation("org.redisson:redisson:3.23.5")
-
-    // Spring Data Redis (Optional - If you want to test it)
-    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive:3.1.0")
-
     // Jedis (Non-Reactive Redis Client)
     implementation("redis.clients:jedis:5.1.0")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 }
 
 tasks.test {
